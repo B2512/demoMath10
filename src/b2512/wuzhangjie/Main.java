794cd9b99d3ed5c5838a317e23d46b92d1eb838a
99283 <99283791@qq.com>
2019-10-17 23:28:28 +0800

    add 吴章杰

--- src/b2512/wuzhangjie/Main.java
+++ src/b2512/wuzhangjie/Main.java
@@ -3,13 +3,13 @@ package b2512.wuzhangjie;
 public class Main {
     public static void main(String[] args) {
         int zongshu = 0;//总数
-        int cheng = 1;//成兔
-        int you = 0;//幼兔
+        int cheng = 0;//成兔
+        int you = 1;//幼兔
         for (int i=1;i<=12;i++){//从1月开始 最大12个月
             zongshu = cheng+you;//总数等于(成兔+幼兔)
             System.out.println(i+"个月的兔子总数为:"+zongshu);
-            cheng = you;//成兔 = 幼兔
-            you = zongshu; //幼兔 = 当前的总数
+            you = cheng;//成兔 = 幼兔
+            cheng = zongshu; //幼兔 = 当前的总数
         }
     }
 }
